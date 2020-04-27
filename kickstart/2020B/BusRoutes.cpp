68c80c0b7670cbb4b60831d403d4d745cb04ce59
jk <jk.hao@pku.edu.cn>
2020-04-27 23:15:45 +0800

    20200427

--- kickstart/2020B/BusRoutes.cpp
+++ kickstart/2020B/BusRoutes.cpp
@@ -1,9 +1,11 @@
 /*
 tm的看错题了, 我以为n是1e12, 原来d是1e12
+long long 没注意...
+这玩意多tm简单啊, 第一遍的思路
 */
+
 #include <cstdio>
 #include <algorithm>
-
 using namespace std;
 
 int t, n;
@@ -16,34 +18,14 @@ void read() {
         scanf("%lld", &a[i]);
 }
 
-bool check(int x) {
-    for(int i=1; i<n; i++) {
-        if(x%a[i] == 0)
-            x = x/a[i];
-        else
-            x = x/a[i] + 1;
-        x *= a[i];
-
-        if(x > d)
-            return false;
-    }
-    return true;
-}
 
 long long solve() {
-    long long l = 1;
-    long long r = d/a[0] + 1;
-    while(true) {
-        if(l == r-1)
-            break;
-        int mid = (l+r) / 2;
-        if(check(mid*a[0]))
-            l = mid;
-        else
-            r = mid;
-    }
-    return l * a[0];
-}  
+    long long ans = d;
+    for(int i=n-1; i>=0; i--)
+        d = d / a[i] * a[i];
+    return d;
+}
+
 
 int main() {
     freopen("input.txt", "r", stdin);
@@ -54,3 +36,61 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+
+// #include <cstdio>
+// #include <algorithm>
+
+// using namespace std;
+
+// int t, n;
+// long long d;
+// long long a[1005];
+
+// void read() {
+//     scanf("%d %lld", &n, &d);
+//     for(int i=0; i<n; i++)
+//         scanf("%lld", &a[i]);
+// }
+
+// bool check(long long x) {
+//     for(int i=0; i<n; i++) {
+//         if(x%a[i] == 0)
+//             x = x/a[i];
+//         else
+//             x = x/a[i] + 1;
+//         x *= a[i];
+
+//         if(x > d)
+//             return false;
+//     }
+//     return true;
+// }
+
+// long long solve() {
+//     long long l = 1;
+//     long long r = d + 1;
+//     while(true) {
+//         if(l == r-1)
+//             break;
+//         int mid = (l+r) / 2;
+//         if(check(mid))
+//             l = mid;
+//         else
+//             r = mid;
+//     }
+//     return l;
+// }  
+
+// int main() {
+//     freopen("input.txt", "r", stdin);
+//     scanf("%d", &t);
+//     for(int ce=1; ce<=t; ce++) {
+//         read();
+//         printf("Case #%d: %lld\n", ce, solve());
+//     }
+//     return 0;
+// }
--- kickstart/2020B/RobotPathDecoding.cpp
+++ kickstart/2020B/RobotPathDecoding.cpp
@@ -6,24 +6,29 @@ using namespace std;
 int t;
 
 int f() {
-    char c = getchar();
+    char c;
+    c = getchar();
     int ans = 0;
     int sn = 0;
     int sx = 1, sy = 1;
-    if(c<='9' && c>='0') {
+    while(c=='N' || c=='S' || c=='E' || c=='W') {
+        if(c == 'N')
+            sx -= 1;
+        else if(c == 'S')
+            sx += 1;
+        else if(c == 'E')
+            sy -= 1;
+        else if(c == 'W')
+            sy += 1;
+        c = getchar();
+    }
+    while(c<='9' && c>='0') {
         sn *= 10;
         sn += c-'0';
+        c = getchar();
     }
-    else if(c == '(')
+    if(c == '(')
         ans += f();
-    else if(c == 'N')
-        sx -= 1;
-    else if(c == 'S')
-        sx += 1;
-    else if(c == 'E')
-        sy -= 1;
-    else if(c == 'W')
-        sy += 1;
     else
         return sn * ans;
 }
@@ -32,6 +37,7 @@ int f() {
 int main() {
     freopen("input.txt", "r", stdin);
     scanf("%d", &t);
+    getchar();
     for(int ce=1; ce<=t; ce++) {
         int ans = f();
         printf("Case #%d: %d\n", ce, ans);
